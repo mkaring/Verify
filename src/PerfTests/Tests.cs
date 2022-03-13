0ecf44c4963c46b2e72afb119fdf94b652e8f104
Simon Cropp <simon.cropp@gmail.com>
2022-03-13 18:16:17 +1100

    .

--- src/PerfTests/Tests.cs
+++ src/PerfTests/Tests.cs
@@ -2,7 +2,25 @@
 public class Tests
 {
     [Fact]
-    public Task Single()
+    public Task Test1()
+    {
+        var person = new Person
+        {
+            GivenNames = "John",
+            FamilyName = "Smith",
+            Spouse = "Jill",
+            Address = new()
+            {
+                Street = "1 Puddle Lane",
+                Suburb = "Gotham",
+                Country = "USA"
+            }
+        };
+        return Verify(person);
+    }
+
+    [Fact]
+    public Task Test2()
     {
         var person = new Person
         {
